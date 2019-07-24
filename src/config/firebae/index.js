994f263a35bbe03702efac213d6b682da0c638d7
Syed Hasnain Zaidi <39019111+syedzaidi95@users.noreply.github.com>
2019-07-24 13:08:25 +0500

    Update index.js

--- src/config/firebae/index.js
+++ src/config/firebae/index.js
@@ -1,13 +1,5 @@
 import * as firebase from 'firebase';
 // import 'firebase/database';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAXMYmDwgPfg9PZkDbz5BBJmE_YXi_g4P4",
-    authDomain: "foodnow95.firebaseapp.com",
-    databaseURL: "https://foodnow95.firebaseio.com",
-    projectId: "foodnow95",
-    storageBucket: "foodnow95.appspot.com",
-    messagingSenderId: "426348569359",
-    appId: "1:426348569359:web:37bb7c97f466aa27"
-  };
+const firebaseConfig = {};
 export default firebase.initializeApp(firebaseConfig)
